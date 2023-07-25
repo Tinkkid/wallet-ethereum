ebf2f60cfaa4d534bd494218bd7b3188f4c8f753
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-07-25 17:30:10 +0300

    Added Formik for inputs

--- src/components/ConnectWallet/ConnectWallet.jsx
+++ src/components/ConnectWallet/ConnectWallet.jsx
@@ -3,12 +3,12 @@ import {  Web3Button, Web3Modal } from '@web3modal/react'
 import { configureChains, createConfig, WagmiConfig } from 'wagmi'
 import { arbitrum, mainnet, sepolia } from 'wagmi/chains'
 import { ethers } from "ethers";
+
 import { useState } from "react";
-import { useWeb3Modal } from '@web3modal/react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const chains = [arbitrum, mainnet, sepolia]
 const projectId = import.meta.env.VITE_APP_PROJECT_KEY;
-
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 const wagmiConfig = createConfig({
@@ -19,15 +19,16 @@ const wagmiConfig = createConfig({
 
 const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
-
-
 const ConnectWallet = () => {
-  const { open, close } = useWeb3Modal();
-
-  const [userBalance, setUserBalance] = useState('');
+  const [userBalance, setUserBalance] = useState(' ');
   const [error, setError] = useState(null);
   const [account, setAccount] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+
+  const initialValues = {
+  address: '',
+  amount: '',
+};
 
   const connect = async () => {
     if (window.ethereum) {
@@ -57,51 +58,52 @@ const ConnectWallet = () => {
          });
   }
   
-  // console.log(userBalance)
   const customBalance = Number(userBalance).toFixed(3);
-  // const customBalance = userBalance.toFixed(3);
 
-  const sendTransaction = async (e) => {
-   
-      e.preventDefault();
-      let adress = String(e.target.to_adress.value)
-      // console.log(adress)
-     let res = String(e.target.amount.value);
-      let amount = ethers.parseEther(res)
-
+  const handleSubmit = (values, { resetForm }) => {
       let params = [
         {
          from: String(account),
-          to: adress,
+          to: String(values.address),
           gas: Number(21000).toString(16),
           gasPrice: Number(25000).toString(16),
-          value:amount.toString(16)
+          value:(ethers.parseEther(values.amount)).toString(16)
         },
       ];
       
-    await window.ethereum.request({
+     window.ethereum.request({
         method: "eth_sendTransaction",
         params
-      }).catch(error => console.log(error));
-   }
+    }).catch(error => console.log(error));
+
+    console.log(values);
+    resetForm();
+  };
+
 
   return (
     <>
       <WagmiConfig config={wagmiConfig}>
         <Web3Button onClick={() => connect()} />
-        <h3>Balance: {
-          isOpen && <span> {customBalance} </span>}
-        </h3>
+        <h3>Balance: {isOpen && <span> {customBalance} </span>}</h3>
       </WagmiConfig>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-      <form onSubmit={sendTransaction}>
-        <label>Enter transaction adress:</label>
-        <input type="text" placeholder="Recipient adress" name="to_adress" />
-        <input placeholder="Amount in ETH" name="amount" />
-        <input type="submit" value="submit" />
-      </form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+         <Form>
+        <label htmlFor="address">Adress</label>
+        <Field id="address" type="text" name="address" placeholder="Address to" />
+        <label htmlFor="amount">Amount</label>
+        <Field id="amount" type="text" name="amount" placeholder="0.005" />
+        <button type="submit">Submit</button>
+      </Form>
+      </Formik>
     </>
   );
 }
+
+// 0x0890412e7dF4FC959F13e5A8D33EaEaf830d91d4
 
 export default ConnectWallet;
